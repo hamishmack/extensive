24e4217886b0ccf1499088bf21c53430cfa86ff1
Hamish Mackenzie <Hamish.K.Mackenzie@googlemail.com>
2016-09-07 16:32:03 +1200

    Add more output (including one really long line)

--- build.sh
+++ build.sh
@@ -7,6 +7,17 @@ do
     sleep 0.01;
 done;
 
+for i in {1..20000}
+do 
+    echo -n $i The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog.;
+done;
+
+for i in {1..10000}
+do 
+    echo $i The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog.;
+    sleep 0.01;
+done;
+
 cp extensive.cabal /output/
 cp *.hs /output/
 cp *.sh /output/
