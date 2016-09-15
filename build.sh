6429a666ce9a91f030f0c76b96cc30a2a683a3d7
Hamish Mackenzie <Hamish.K.Mackenzie@googlemail.com>
2016-09-15 13:30:27 +1200

    Less logging

--- build.sh
+++ build.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 #  Simple script to make some noise
 
-for i in {1..10000}
+for i in {1..100}
 do 
     echo $i The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog.;
-    sleep 0.01;
+    sleep 0.1;
 done;
 
-for i in {1..200000}
+for i in {1..2000}
 do 
     echo -n $i The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog.;
 done;
 
-for i in {1..10000}
+for i in {1..100}
 do 
     echo $i The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog.;
-    sleep 0.01;
+    sleep 0.1;
 done;
 
 cp extensive.cabal /output/
