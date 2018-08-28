f514846ca0c54544a05f3b8a9e5d62a4460f9aee
Hamish Mackenzie <Hamish.K.Mackenzie@googlemail.com>
2018-08-28 17:54:10 +1200

    Test 1m log outputs

--- build.sh
+++ build.sh
@@ -2,21 +2,21 @@
 #  Simple script to make some noise
 # Testing webhooks again
 
-for i in {1..5000}
+for i in {1..500000}
 do 
     echo $i The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog.;
-    sleep 0.01;
+    # sleep 0.01;
 done;
 
-for i in {1..200}
+for i in {1..20}
 do 
     echo -n $i The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog.;
 done;
 
-for i in {1..5000}
+for i in {1..500000}
 do 
     echo $i The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog.;
-    sleep 0.01;
+    # sleep 0.01;
 done;
 
 cp extensive.cabal /output/
