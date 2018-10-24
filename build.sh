edcd7da3ece18eb86b66e1b0bcc7afe307452542
Hamish Mackenzie <Hamish.K.Mackenzie@googlemail.com>
2018-10-24 15:15:07 +1300

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,10 @@
 #  Simple script to make some noise
 # Testing webhooks again
 
+echo A=$A
+echo B=$B
+echo C=$C
+
 for i in {1..50}
 do 
     echo $i The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog.;
