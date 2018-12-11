504726abede889f885b7c7244df3a57c19b888ad
Hamish Mackenzie <Hamish.K.Mackenzie@googlemail.com>
2018-12-11 15:39:09 +1300

    Update build.sh

--- test2/build.sh
+++ test2/build.sh
@@ -3,8 +3,15 @@
 cat /input/test/C.txt
 echo "C = $C"
 
+printf "------------------------------\r"
+for a in {1..30}; do
+  printf "#";
+  sleep 1;
+done;
+printf "\n"
+
 printf "        <- counting\r"
-for a in {1..1000}; do
+for a in {1..100}; do
   printf "$a\r";
   sleep 1;
 done;
